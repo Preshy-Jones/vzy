8feab07cfa8df6665057172186a85be1dcd70662
Precious Adedibu <adedibuprecious@gmail.com>
2024-02-15 16:03:11 +0100

    update

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
 
@@ -8,56 +8,58 @@ export class StripeService {
 
   handleWebhook(payload: any) {
     console.log(payload);
+    console.log(this.configService.get('STRIPE_SECRET_KEY'));
 
-    // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+    const stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY'));
 
-    // const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!;
+    const webhookSecret: string = this.configService.get(
+      'STRIPE_WEBHOOK_SECRET',
+    );
 
-    // const sig = req.headers['stripe-signature']!;
+    const sig = req.headers['stripe-signature']!;
 
-    // let event: Stripe.Event;
+    let event: Stripe.Event;
 
-    // try {
-    //   const body = await buffer(req);
-    //   event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
-    // } catch (err) {
-    //   // On error, log and return the error message
-    //   console.log(`❌ Error message: ${(err as any).message}`);
-    //   res.status(400).send(`Webhook Error: ${(err as any).message}`);
-    //   return;
-    // }
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, webhookSecret);
+    } catch (err) {
+      // On error, log and return the error message
+      console.log(`❌ Error message: ${(err as any).message}`);
+      throw new HttpException((err as any).message, 400);
+      return;
+    }
 
-    // // Successfully constructed event
-    // console.log('✅ Success:', event.id);
+    // Successfully constructed event
+    console.log('✅ Success:', event.id);
 
-    // // Cast event data to Stripe object
-    // if (event.type === 'payment_intent.succeeded') {
-    //   const stripeObject: Stripe.PaymentIntent = event.data
-    //     .object as Stripe.PaymentIntent;
-    //   console.log(`💰 PaymentIntent status: ${stripeObject.status}`);
-    //   await prisma.payment.update({
-    //     where: {
-    //       clientSecret: stripeObject.client_secret!,
-    //     },
-    //     data: {
-    //       status: 'completed',
-    //     },
-    //   });
-    // } else if (event.type === 'payment_intent.payment_failed') {
-    //   const stripeObject = event.data.object as Stripe.PaymentIntent;
-    //   console.log(`💰 PaymentIntent status: ${stripeObject.status}`);
+    // Cast event data to Stripe object
+    if (event.type === 'payment_intent.succeeded') {
+      const stripeObject: Stripe.PaymentIntent = event.data
+        .object as Stripe.PaymentIntent;
+      console.log(`💰 PaymentIntent status: ${stripeObject.status}`);
+      await prisma.payment.update({
+        where: {
+          clientSecret: stripeObject.client_secret!,
+        },
+        data: {
+          status: 'completed',
+        },
+      });
+    } else if (event.type === 'payment_intent.payment_failed') {
+      const stripeObject = event.data.object as Stripe.PaymentIntent;
+      console.log(`💰 PaymentIntent status: ${stripeObject.status}`);
 
-    //   await prisma.payment.update({
-    //     where: {
-    //       clientSecret: stripeObject.client_secret!,
-    //     },
-    //     data: {
-    //       status: 'failed',
-    //     },
-    //   });
-    // } else {
-    //   console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
-    // }
+      await prisma.payment.update({
+        where: {
+          clientSecret: stripeObject.client_secret!,
+        },
+        data: {
+          status: 'failed',
+        },
+      });
+    } else {
+      console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
+    }
 
     // // Return a response to acknowledge receipt of the event
     // res.json({ received: true });
